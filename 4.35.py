7f22625f2db45f688a600e9530445f79513c2953
DE1C1DE <159756754+DE1C1DE@users.noreply.github.com>
2024-05-23 11:05:40 +0300

    Create 4.35.py

---
+++ 4.35.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+from typing import List, Tuple
+
+def fill_specializations(specializations: List[Tuple[str, str]]):
+    spec_dict = defaultdict(list)
+    for spec, name in specializations:
+        spec_dict[spec].append(name)
+    return dict(spec_dict)
